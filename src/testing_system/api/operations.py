087756692d83900532822584eda151c5e47721ac
Std1o <37378410+Std1o@users.noreply.github.com>
2022-03-04 09:31:48 +0300

    fastapi filter

--- src/testing_system/api/operations.py
+++ src/testing_system/api/operations.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 from fastapi import APIRouter
-from ..models.operations import Operation
+from ..models.operations import Operation, OperationKind
 from ..database import get_session
 from fastapi import Depends
 from ..services.operations import OperationService
@@ -8,5 +8,5 @@ from ..services.operations import OperationService
 router = APIRouter(prefix='/operations')
 
 @router.get('/', response_model=List[Operation])
-def get_operations(service: OperationService = Depends()):
-    return service.get_list()
+def get_operations(kind: Optional[OperationKind] = None, service: OperationService = Depends()):
+    return service.get_list(kind=kind)
