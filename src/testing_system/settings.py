44fe19745e6679930930cc2651135c32492b6c9b
Std1o <37378410+Std1o@users.noreply.github.com>
2022-03-05 10:34:36 +0300

    fastapi auth

--- src/testing_system/settings.py
+++ src/testing_system/settings.py
@@ -5,4 +5,8 @@ class Settings(BaseSettings):
     server_port: int = 8000
     database_url: str = 'sqlite:///./database.sqlite3'
 
+    jwt_sercret: str = 'I8HheOD_Ue-xmEH1yo8OgxvRLUPbh7ujm2zsoHyjaM4'
+    jwt_algorithm: str = 'HS256'
+    jwt_expiration: int = 3600
+
 settings = Settings()
