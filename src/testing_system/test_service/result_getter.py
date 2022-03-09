b2ce429a3d5ee359c1a44ae77fac279752397a84
Std1o <37378410+Std1o@users.noreply.github.com>
2022-03-09 11:49:26 +0300

    results deletion

---
+++ src/testing_system/test_service/result_getter.py
@@ -0,0 +1,12 @@
+from fastapi import Depends
+
+from testing_system.test_service.base_test_service import BaseTestService
+from sqlalchemy.orm import Session
+from .. import tables, constants
+from ..database import get_session
+
+
+class TestResultService(BaseTestService):
+    def __init__(self, session: Session = Depends(get_session)):
+        super().__init__(session)
+        self.session = session
