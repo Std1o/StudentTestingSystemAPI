c6cd0375c0d9e6a780bd2876c5961b960f55d242
Std1o <37378410+Std1o@users.noreply.github.com>
2022-03-10 02:23:44 +0300

    upd

--- src/testing_system/__main__.py
+++ src/testing_system/__main__.py
@@ -1,7 +1,13 @@
 import uvicorn
 from .settings import settings
 
-uvicorn.run('testing_system.app:app',
-            host=settings.server_host,
-            port=settings.server_port,
-            reload=True)
+
+def main():
+    uvicorn.run('testing_system.app:app',
+                host=settings.server_host,
+                port=settings.server_port,
+                reload=True)
+
+
+if __name__ == "__main__":
+    main()
