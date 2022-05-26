f646e3b45d99b3c0f26d4d83488d461330c4e302
Std1o <37378410+Std1o@users.noreply.github.com>
2022-05-26 04:49:23 +0300

    Update result_getter.py

--- src/testing_system/test_service/result_getter.py
+++ src/testing_system/test_service/result_getter.py
@@ -17,12 +17,12 @@ class TestResultService(TestSearchingService):
         super().__init__(session)
         self.session = session
 
-    def get_user_answer(self, answer_id: int) -> tables.UsersAnswers:
-        statement = select(tables.UsersAnswers).filter_by(answer_id=answer_id)
+    def get_user_answer(self, answer_id: int, user_id: int) -> tables.UsersAnswers:
+        statement = select(tables.UsersAnswers).filter_by(answer_id=answer_id, user_id=user_id)
         return self.session.execute(statement).scalars().first()
 
-    def get_answer_result(self, ans: Answer) -> AnswerResult:
-        user_answer = self.get_user_answer(ans.id)
+    def get_answer_result(self, ans: Answer, user_id: int) -> AnswerResult:
+        user_answer = self.get_user_answer(ans.id, user_id)
         if not user_answer:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         answer = AnswerResult(is_selected=user_answer.is_selected,
@@ -31,13 +31,13 @@ class TestResultService(TestSearchingService):
                               is_right=ans.is_right)
         return answer
 
-    def get_question_score(self, question_id: int) -> float:
-        statement = select(tables.QuestionsResults.score).filter_by(question_id=question_id)
+    def get_question_score(self, question_id: int, user_id: int) -> float:
+        statement = select(tables.QuestionsResults.score).filter_by(question_id=question_id, user_id=user_id)
         return self.session.execute(statement).scalars().first()
 
-    def get_question_result(self, question: Question, answers: List[AnswerResult]) -> QuestionResult:
+    def get_question_result(self, question: Question, answers: List[AnswerResult], user_id: int) -> QuestionResult:
         question_result = QuestionResult(answers=answers,
-                                         score=self.get_question_score(question.id),
+                                         score=self.get_question_score(question.id, user_id),
                                          id=question.id,
                                          question=question.question)
         return question_result
@@ -52,8 +52,8 @@ class TestResultService(TestSearchingService):
         for question in test.questions:
             answers: List[AnswerResult] = []
             for ans in question.answers:
-                answers.append(self.get_answer_result(ans))
-            questions.append(self.get_question_result(question, answers))
+                answers.append(self.get_answer_result(ans, user_id))
+            questions.append(self.get_question_result(question, answers, user_id))
         score = self.get_test_score(user_id, test_id)
         if not score:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
