10e44c64ffafb6fc9158d8350b709b9d8bcff7d9
Ilya <37378410+Std1o@users.noreply.github.com>
2022-10-21 15:38:49 +0300

    upd test_getter.py

--- src/testing_system/test_service/test_getter.py
+++ src/testing_system/test_service/test_getter.py
@@ -1,26 +1,18 @@
 from typing import List
-from fastapi import Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from sqlalchemy import select
+from fastapi import HTTPException, status
 from .base_test_service import BaseTestService
-from .. import tables
-from ..models import tables as tbls
-from ..database import get_session, make_query
+from ..models import tables
+from ..database import make_query, get_list
 from ..models.test import Test, Answer, Question
 
 
 class TestSearchingService(BaseTestService):
-    def __init__(self, session: Session = Depends(get_session)):
-        super().__init__(session)
-        self.session = session
 
     def get_questions(self, test_id: int) -> List[tables.Questions]:
-        statement = select(tables.Questions).filter_by(test_id=test_id)
-        return self.session.execute(statement).scalars().all()
+        return get_list("SELECT * FROM questions where test_id=?", tables.Questions, test_id)
 
     def get_answers(self, question_id: int) -> List[tables.Answers]:
-        statement = select(tables.Answers).filter_by(question_id=question_id)
-        return self.session.execute(statement).scalars().all()
+        return get_list("SELECT * FROM answers where question_id=?", tables.Answers, question_id)
 
     def convert_test_from_table(self, test: tables.Test, show_right_ans: bool) -> Test:
         test_dict = dict(test)
@@ -46,7 +38,7 @@ class TestSearchingService(BaseTestService):
 
     def get(self, user_id: int, course_id: int, test_id: int, for_result: bool) -> Test:
         self.check_accessibility(user_id, course_id)
-        test = make_query("SELECT * FROM tests where id=? LIMIT 1", tbls.Test, test_id)
+        test = make_query("SELECT * FROM tests where id=? LIMIT 1", tables.Test, test_id)
         if not test:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         return self.convert_test_from_table(test, for_result)
